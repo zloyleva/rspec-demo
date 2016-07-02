b6beabd64e9764e6ca0f7402b920c3e86925bef9
zloyleva <zloyleva@gmail.com>
2016-07-02 20:05:09 +0300

    Add hero info test

--- hero_spec.rb
+++ hero_spec.rb
@@ -18,4 +18,10 @@ describe Hero do
 
 		expect(hero.power_down).to eq 90
 	end
+
+	it "displays full hero info" do
+		hero = Hero.new 'mike'
+
+		expect(hero.hero_info).to eq 'Mike has a health of 100'
+	end
 end
